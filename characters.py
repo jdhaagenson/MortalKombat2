1de848243b5f418456bbc1685bd1852971cc4d86
Jordan Haagenson <jdhaagenson@gmail.com>
2020-05-26 02:32:54 -0700

    add gitignore

--- characters.py
+++ characters.py
@@ -3,13 +3,29 @@
 from itertools import cycle
 
 import pygame as pg
+pg.init()
+
+spritesheets = {
+    'blood': "SNES_MK2_blood.png",
+    'jade': "SNES_MK2_jade.png",
+    'jax': "SNES_MK2_jax.png",
+    'kintaro': "SNES_MK2_kintaro.png",
+    'kitana': "SNES_MK2_kitana.png",
+    'kunglao': "SNES_MK2_kunglao.png",
+    'liukang': "SNES_MK2_liuKang.png",
+    'mileena': "SNES_MK2_mileena.png",
+    'reptile': "SNES_MK2_reptile.png",
+    'scorpion': "SNES_MK2_scorp.png",
+    'shang': "SNES_MK2_shang.png",
+    'subzero': "SNES_MK2_subz.png"
+}
 
 
 class Character:
     def __init__(self, spritesheet):
         self.spritesheet = spritesheet
         self.images = [[],[],[]]
-        self.image = pg.image.load(self.images[0][0]).convert_alpha()
+        self.image = load_image(self.images[0][0])
         self.rect = self.image.get_rect()
         self.position = self.rect.x 
         self.facing = 'right'
@@ -17,3 +33,7 @@ class Character:
         self.image = cycle(self.images[0][0])
         if self.facing == 'left':
             self.image 
+    @staticmethod        
+    def load_image(image):
+        return pg.image.load(image).convert_alpha()
+    
